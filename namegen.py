9f6ba202ab82cb763bcaa24b49469ca8e166d5a0
tsaicch <90241325+tsaicch@users.noreply.github.com>
2021-12-20 03:18:53 +0000

    Update namegen.py

--- namegen.py
+++ namegen.py
@@ -2,13 +2,27 @@ import argparse
 
 def get_parser():
     parser = argparse.ArgumentParser(description='my description')
-    parser.add_argument('ip', type=str)
-    parser.add_argument('port', type=int)
+    parser.add_argument('cloudProvider', type=str)
+    parser.add_argument('env', type=str)
+    parser.add_argument('resourceType', type=str)
+#    parser.add_argument('resourceNum', type=int)
+    return parser
+def get_parser2():
+    parser = argparse.ArgumentParser(description='my description')
+    parser.add_argument('-cp', '--cloudprovider', type=str)
+    parser.add_argument('-env', '--environment', type=str)
+    parser.add_argument('-rt', '--resourcetype', type=str)
+#    parser.add_argument('resourceNum', type=int)
     return parser
 
 if __name__ == '__main__':
-    parser = get_parser()
+    cloudProviderMap={"google":"g","azure":"a","awx":"w"}
+    envMap={"production":"p", "uat":"u", "dev":"d"}
+    resourcerMap={"computing_engine":"vm","cloud_storage":"csg","cloud_network":"cng"}
+    
+    parser = get_parser2()
     args = parser.parse_args()
-    print(args.ip + ':' + str(args.port))
-    print(type(args.ip))
-    print(type(args.port))
+    a=cloudProviderMap[args.cloudprovider]
+    b=envMap[args.environment]
+    c=resourcerMap[args.resourcetype]
+    print(a + b + c)
